50d46a429dc32bb6a7447aabae99ebbc2811ba63
dheerajreddybadduri <115022128+dheerajreddybadduri@users.noreply.github.com>
2022-12-11 21:24:41 -0600

    Update LoginPage.xaml.cs

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -15,9 +15,7 @@ public partial class LoginPage : ContentPage
         var uid = await pmService.loginUser(email, pass);
         if (uid != null)
         {
-            await SecureStorage.SetAsync("uid", uid);
-            await Navigation.PushAsync(new MainPage());
-        }
+           
         else
         {
             await DisplayAlert("Alert", "Login Error, Please try again", "OK");
