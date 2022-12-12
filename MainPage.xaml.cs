704e9e047fa34788bb2f0c8458551c21aff18cdf
dheerajreddybadduri <115022128+dheerajreddybadduri@users.noreply.github.com>
2022-12-11 21:27:18 -0600

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,8 +6,9 @@ public partial class MainPage : ContentPage
 
 	public MainPage()
 	{
-		
-	}
+		InitializeComponent();
+		BindingContext = App.vm;
+		}
 
 	private void Button_Clicked(object sender, EventArgs e)
 	{
